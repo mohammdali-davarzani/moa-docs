62191be43fccbb361b4bf288e45bdc0a96688105
Mohammadali <63012627+mohammdali-davarzani@users.noreply.github.com>
2024-05-24 22:52:48 +0330

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -78,7 +78,7 @@ const config = {
             position: 'left',
             label: 'Docs',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          // {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/mohammdali-davarzani/moa-docs',
             label: 'GitHub',
